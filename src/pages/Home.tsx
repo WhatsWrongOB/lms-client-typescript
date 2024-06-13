ba15c8296ee73f17a053f1b92e14014849bf2532
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2024-06-13 17:01:04 +0500

    fix cookie

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, FormEvent } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
