8db6b8aee7596e8168067c1c754c85a294b12e56
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2024-06-21 11:44:15 +0500

    .env

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { User } from "../pages/Home";
 
+
 const ProtectedRoute = () => {
   const token: string | null = localStorage.getItem("token");
 
   return token ? <Outlet /> : <Navigate to="/" />;
 };
+
 
 const AdminRoute = () => {
   const storedUser: string | null = localStorage.getItem("CurrentUser");
